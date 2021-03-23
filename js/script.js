b4cd6756fab180358ff86f223f3b889948e93082
Akim <akimserverov75@gmail.com>
2021-03-23 20:09:02 +0300

    add form

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@
 
 window.addEventListener(`DOMContentLoaded`, (e) => {
 
+
+    //trailer
     const button = document.querySelector(`#button`),
           modal = document.querySelector(`.trailer`),
           tagVideo = document.querySelector(`.trailer_video`),
@@ -43,5 +45,41 @@ window.addEventListener(`DOMContentLoaded`, (e) => {
          
     });
 
+
+    //pre-order
+
+    const preOrder = document.querySelector(`.preOrder`),
+          btnPreOrder = document.querySelector(`.information_wrapper_buttons_button_1`),
+          closePreOrder = document.querySelector(`.preOrder_wrapper_close`);
+
+    function showFormPreOrder() {
+        preOrder.classList.add(`show`);
+        document.body.style.overflow = `hidden`;
+    }
+
+    function hideFormPreOrder() {
+        preOrder.classList.remove(`show`);
+        document.body.style.overflow = ``;
+    }
+
+    btnPreOrder.addEventListener(`click`, (e) => {
+        showFormPreOrder();
+    });
+
+    closePreOrder.addEventListener(`click`, (e) => {
+        hideFormPreOrder();
+    });
+
+    preOrder.addEventListener(`click`, (e) => {
+        if (e.target === preOrder && e.target.classList.contains(`show`)) {
+            hideFormPreOrder();
+        }
+    });
+
+    document.addEventListener(`keydown`, (e) => {
+        if (e.code === `Escape` && preOrder.classList.contains(`show`)) {
+            hideFormPreOrder();
+        }
+    });
 
 });
